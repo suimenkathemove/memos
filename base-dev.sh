9d498a94d9f2022361e1e46ee57f26246376da38
emgniddikur <yoshiharu2580@gmail.com>
2022-11-20 08:27:48 +0900

    add base-dev.sh

---
+++ base-dev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# TODO
+PROJECT_NAME="TODO"
+
+DEV_DIR="${HOME}/development/emgniddikur"
+
+cd "$DEV_DIR/$PROJECT_NAME"
+
+if tmux has-session -t "$PROJECT_NAME"; then
+  tmux attach-session -t "$PROJECT_NAME"
+else
+  tmux new-session -s "$PROJECT_NAME" -d
+  tmux attach-session -t "$PROJECT_NAME"
+fi
