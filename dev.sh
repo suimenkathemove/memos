10023804c6180c9fcfa9a883aa32bc716b3cfb80
suimenkathemove <yoshiharu2580@gmail.com>
2022-12-12 18:53:56 +0900

    fix dev.sh

--- dev.sh
+++ dev.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# EDIT THIS
 PROJECT_NAME="articles"
 
+# EDIT THIS
 DEV_DIR_PATH="${HOME}/development/suimenkathemove"
 
 cd "$DEV_DIR_PATH/$PROJECT_NAME" || exit
@@ -9,6 +11,8 @@ cd "$DEV_DIR_PATH/$PROJECT_NAME" || exit
 if tmux has-session -t "$PROJECT_NAME"; then
   tmux attach-session -t "$PROJECT_NAME"
 else
+  # EDIT THIS
   tmux new-session -s "$PROJECT_NAME" -d
+
   tmux attach-session -t "$PROJECT_NAME"
 fi
